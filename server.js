e441a242a973ff4d9acfea6dbbef3921bba7f082
Nice <nicenicegame@gmail.com>
2021-08-11 23:20:03 +0700

    change to express server

---
+++ server.js
@@ -0,0 +1,47 @@
+require('dotenv').config()
+const express = require('express')
+const axios = require('axios')
+const cors = require('cors')
+
+const appKey = process.env.APP_KEY
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.post('/login', async (req, res) => {
+  try {
+    const response = await axios.post('https://myapi.ku.th/auth/login', req.body, {
+      headers: {
+        'app-key': appKey
+      }
+    })
+    res.json(response.data)
+  } catch (e) {
+    res.json(e)
+  }
+})
+
+app.get('/getSchedule', async (req, res) => {
+  const accessToken = req.headers['accesstoken']
+  const { stdId } = req.query
+  try {
+    const response = await axios.get('https://myapi.ku.th/std-profile/getGroupCourse', {
+      params: {
+        academicYear: 2564,
+        semester: 1,
+        stdId
+      },
+      headers: {
+        "x-access-token": accessToken,
+        'app-key': appKey
+      }
+    })
+    res.json(response.data.results[0].course)
+  } catch (e) {
+    res.json(e)
+  }
+})
+
+app.listen(process.env.PORT, () => console.log('Connected'))
