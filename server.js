2046a14da409a3fb6068777505d584f0c99c59dc
Puvana Swatvanith <puvana.jp36@gmail.com>
2024-06-25 12:41:23 +0700

    fix: remove error log (causes H10) (#12)
    ## Describe your changes
    
    Fix H10 App Crashed
    
    ## Issue ticket number and link
    
    ## Type of change
    
    Please delete options that are not relevant.
    
    - [x] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [x] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] This change requires a documentation update
    
    ## Checklist before requesting a review
    - [x] I have performed a self-review of my code
    - [ ] If it is a core feature, I have added thorough tests.
    - [ ] Do we need to implement analytics?
    - [ ] Will this be part of a product update? If yes, please write one
    phrase about this update.

--- server.js
+++ server.js
@@ -1,350 +1,357 @@
-if(process.env.NODE_ENV !== "production"){
-  require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
-const express = require('express')
-const axios = require('axios')
-const fs = require('fs')
-const crypto = require('crypto')
+const express = require("express");
+const axios = require("axios");
+const fs = require("fs");
+const crypto = require("crypto");
 
-const loginLink = 'https://myapi.ku.th/auth/login'
-const getScheduleLink = 'https://myapi.ku.th/std-profile/getGroupCourse'
-const checkGradesLink = 'https://myapi.ku.th/std-profile/checkGrades'
-const publicCBP = 'https://myapi.ku.th/common/publicCBP'
-const sheetLink = process.env.SHEET_LINK
-const kuPublicKey = process.env.KU_PUBLIC_KEY.replace(/\\n/gm, '\n')
-const appKey = process.env.APP_KEY
+const loginLink = "https://myapi.ku.th/auth/login";
+const getScheduleLink = "https://myapi.ku.th/std-profile/getGroupCourse";
+const checkGradesLink = "https://myapi.ku.th/std-profile/checkGrades";
+const publicCBP = "https://myapi.ku.th/common/publicCBP";
+const sheetLink = process.env.SHEET_LINK;
+const kuPublicKey = process.env.KU_PUBLIC_KEY.replace(/\\n/gm, "\n");
+const appKey = process.env.APP_KEY;
 
-const app = express()
+const app = express();
 
-let raw_subject = fs.readFileSync('./data/subject.json')
-const subjectJson = JSON.parse(raw_subject)
+let raw_subject = fs.readFileSync("./data/subject.json");
+const subjectJson = JSON.parse(raw_subject);
 
-let raw_major = fs.readFileSync('./data/newGenEd.json')
-const genEdJson = JSON.parse(raw_major)
+let raw_major = fs.readFileSync("./data/newGenEd.json");
+const genEdJson = JSON.parse(raw_major);
 
-app.use( (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With,accesstoken');
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With,accesstoken"
+  );
 
-  if ('OPTIONS' == req.method) {
+  if ("OPTIONS" == req.method) {
     res.send(200);
-  }
-  else {
+  } else {
     next();
   }
-})
+});
 
-app.use(express.json())
+app.use(express.json());
 
-app.post('/login', async (req, res) => {
+app.post("/login", async (req, res) => {
+  const encodedBody = {
+    username: encodeString(req.body.username),
+    password: encodeString(req.body.password),
+  };
+
+  let response;
   try {
-    const encodedBody = {
-      username: encodeString(req.body.username),
-      password: encodeString(req.body.password)
-    }
-    const response = await axios.post(loginLink, encodedBody, {
+    response = await axios.post(loginLink, encodedBody, {
       headers: {
-        'app-key': appKey
-      }
-    })
-    // using = using + 1;
-    const student = response.data.user.student
-    const { studentYear, facultyNameEn, majorNameEn, stdId, majorCode } = student
-    
-    // if (stdCache.indexOf(stdId) <= -1){
-    //   stdCache.push(stdId);
-    // }
-    
-    if (response.data.code == "success") {
-      console.log('Login/ success', facultyNameEn, ",", majorCode, majorNameEn, ",", studentYear);
-      try {
-        axios.post(sheetLink, 
-          {
-            "facultyNameEn": facultyNameEn,
-            "majorNameEn": majorCode + " " + majorNameEn, 
-            "studentYear": studentYear
-          }
-        )
-        const concat = facultyNameEn + " , " + majorCode + " " + majorNameEn + " , " + studentYear
-        console.log("Sheet/ [", concat, "] has been added to Sheet")
-      }
-      catch (e) {
-        console.log(e)
-        console.log("Sheet/ Fail to call API.")
-      }
-      if (process.env.CBP_FLAG === "true") {
-        try{
-          axios.post(publicCBP,
-            {
-              keys: [["gcd", stdId]]
-            },
-            {
-              headers: {
-                'app-key': 'txCR5732xYYWDGdd49M3R19o1OVwdRFc',
-                'x-access-token': response.data.accesstoken
-              },
-            }
-          )
-        } catch (e) {
-          console.log("PublicCBP err: ", e)
-        }
-      }
-    }
-    
-    res.json(response.data)
-    console.log("Login/ Done")
+        "app-key": appKey,
+      },
+    });
+  } catch (error) {
+    res.status(error.response.status).json(error.response?.data);
+    console.log("Login/ Fail ", error.response?.data);
+    return;
+  }
 
+  const student = response.data.user.student;
+  const { studentYear, facultyNameEn, majorNameEn, stdId, majorCode } = student;
+
+  console.log(
+    "Login/ Success",
+    facultyNameEn,
+    ",",
+    majorCode,
+    majorNameEn,
+    ",",
+    studentYear
+  );
+  try {
+    axios.post(sheetLink, {
+      facultyNameEn: facultyNameEn,
+      majorNameEn: majorCode + " " + majorNameEn,
+      studentYear: studentYear,
+    });
   } catch (e) {
-    try{
-      res.status(e.response.status).json(e)
-      console.log("Login/ Fail, success ku api")
-    } catch {
-      res.status(400).json({"code" :"Fail to login"})
-      console.log("Login/ Fail, unsuccess ku api")
+    console.log("Sheet/ Fail to call API.");
+  }
+
+  if (process.env.CBP_FLAG === "true") {
+    try {
+      axios.post(
+        publicCBP,
+        {
+          keys: [["gcd", stdId]],
+        },
+        {
+          headers: {
+            "app-key": "txCR5732xYYWDGdd49M3R19o1OVwdRFc",
+            "x-access-token": response.data.accesstoken,
+          },
+        }
+      );
+    } catch (e) {
+      console.log("PublicCBP err: ", e);
     }
   }
-})
 
-app.get('/getSchedule', async (req, res) => {
-  const accessToken = req.headers['accesstoken']
-  const { stdId } = req.query
+  res.status(response.status).json(response.data);
+  console.log("Login/ Done");
+});
+
+app.get("/getSchedule", async (req, res) => {
+  const accessToken = req.headers["accesstoken"];
+  const { stdId } = req.query;
   try {
     const header = {
       "x-access-token": accessToken,
-      'app-key': appKey
-    }
+      "app-key": appKey,
+    };
     const response_now = await axios.get(getScheduleLink, {
       params: {
         stdId,
         academicYear: 2567,
         semester: 1,
       },
-      headers: header
-    })
-    console.log('GetSchedule success')
-    console.log(response_now.data)
-    if(("results" in response_now.data)){
-      console.log('GetSchedule/ Done sent data success 2566/2')
-      return res.json(response_now.data.results[0])
-    }
-    else{
+      headers: header,
+    });
+    console.log("GetSchedule success");
+    console.log(response_now.data);
+    if ("results" in response_now.data) {
+      console.log("GetSchedule/ Done sent data success 2566/2");
+      return res.json(response_now.data.results[0]);
+    } else {
       const response_last = await axios.get(getScheduleLink, {
         params: {
           stdId,
           academicYear: 2566,
           semester: 2,
         },
-        headers: header
-      })
-      console.log(response_last.data)
-      if(("results" in response_last.data)){
-        console.log('GetSchedule/ Done sent data*2 success 2566/1')
-        return res.json(response_last.data.results[0])
-      }
-      else{
-        console.log("GetSchedule/ Done but no course found (send default)")
+        headers: header,
+      });
+      console.log(response_last.data);
+      if ("results" in response_last.data) {
+        console.log("GetSchedule/ Done sent data*2 success 2566/1");
+        return res.json(response_last.data.results[0]);
+      } else {
+        console.log("GetSchedule/ Done but no course found (send default)");
         return res.json({
-          "course": [],
-          "peroid_date": "information not available"
-        })
+          course: [],
+          peroid_date: "information not available",
+        });
       }
     }
   } catch (e) {
-    console.log(e.message)
-    try{
-      console.log("GetSchedule/ Fail, success ku api  Send blank table")
+    console.log(e.message);
+    try {
+      console.log("GetSchedule/ Fail, success ku api  Send blank table");
       return res.json({
-        "course": [],
-        "peroid_date": "information not available (Fail to GetSchedule/)"
-      })
+        course: [],
+        peroid_date: "information not available (Fail to GetSchedule/)",
+      });
     } catch {
-      res.status(400).json({"code": "bad request"})
-      console.log("GetSchedule/ Fail, unsuccess ku api")
+      res.status(400).json({ code: "bad request" });
+      console.log("GetSchedule/ Fail, unsuccess ku api");
     }
   }
-})
+});
 
-app.post('/encode', async (req, res) => {
+app.post("/encode", async (req, res) => {
   try {
     const encodedBody = {
       username: encodeString(req.body.username),
-      password: encodeString(req.body.password)
-    }
-    return res.json(encodedBody)
+      password: encodeString(req.body.password),
+    };
+    return res.json(encodedBody);
   } catch (e) {
-    return res.status(400).json({})
+    return res.status(400).json({});
   }
-})
+});
 
 const encodeString = (data) => {
   return crypto
-      .publicEncrypt(
-          {
-              key: kuPublicKey,
-              padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
-          },
-          Buffer.from(data, "utf8")
-      )
-      .toString("base64");
+    .publicEncrypt(
+      {
+        key: kuPublicKey,
+        padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+      },
+      Buffer.from(data, "utf8")
+    )
+    .toString("base64");
 };
 
 const getNeedUnit = (majorCode, stdCode) => {
-  const data = genEdJson[majorCode]
-  if(!data){
-    console.log(`getNeedUnit/ Major Not found ${majorCode}`)
-    return [genEdJson["NotFound"]["0"], 0]
+  const data = genEdJson[majorCode];
+  if (!data) {
+    console.log(`getNeedUnit/ Major Not found ${majorCode}`);
+    return [genEdJson["NotFound"]["0"], 0];
   }
   try {
-    const stdYear = stdCode.substring(0, 2)
+    const stdYear = stdCode.substring(0, 2);
     // 60 65
     // 64 -> 60
     // 65 -> 65
-    var lastest = 60
+    var lastest = 60;
     for (const year in data) {
       if (year <= parseInt(stdYear)) {
-        lastest = year
+        lastest = year;
       }
     }
-    return [data[lastest], lastest]
+    return [data[lastest], lastest];
+  } catch {
+    console.log("throw 60");
+    return [data["60"], 60];
   }
-  catch {
-    console.log("throw 60")
-    return [data["60"], 60]
-  }
-  
-  return [genEdJson["NotFound"]["0"], 0]
-}
+};
 
 const getSubject = (subject_code) => {
-  return subjectJson[subject_code]
-}
+  return subjectJson[subject_code];
+};
 
 const getResultBlock = async (needUnit) => {
   let result = {
-    "Wellness": {
-      "done": 0,
-      "need": needUnit.Wellness,
-      "subjects": []
+    Wellness: {
+      done: 0,
+      need: needUnit.Wellness,
+      subjects: [],
     },
-    "Entrepreneurship": {
-      "done": 0,
-      "need": needUnit.Entrepreneurship,
-      "subjects": []
+    Entrepreneurship: {
+      done: 0,
+      need: needUnit.Entrepreneurship,
+      subjects: [],
     },
-    "Thai_Citizen_and_Global_Citizen": {
-      "done": 0,
-      "need": needUnit.Thai_Citizen_and_Global_Citizen,
-      "subjects": []
+    Thai_Citizen_and_Global_Citizen: {
+      done: 0,
+      need: needUnit.Thai_Citizen_and_Global_Citizen,
+      subjects: [],
     },
-    "Language_and_Communication": {
-      "done": 0,
-      "need": needUnit.Language_and_Communication,
-      "subjects": []
+    Language_and_Communication: {
+      done: 0,
+      need: needUnit.Language_and_Communication,
+      subjects: [],
     },
-    "Aesthetics": {
-      "done": 0,
-      "need": needUnit.Aesthetics,
-      "subjects": []
+    Aesthetics: {
+      done: 0,
+      need: needUnit.Aesthetics,
+      subjects: [],
     },
-    "unitYear": 0,
-  }
-  return result
-}
+    unitYear: 0,
+  };
+  return result;
+};
 
-app.get('/getGenEd', async (req, res) => {
+app.get("/getGenEd", async (req, res) => {
   // body require:
   //  - MajorCode
   //  - x-access-token
   //  - stdCode - idcode # optional
-  
-  const accessToken = req.headers['accesstoken']
-  const { majorCode, stdCode } = req.query
-  
-  try{
-    const [needUnit, unitYear] = getNeedUnit(majorCode, stdCode)
+
+  const accessToken = req.headers["accesstoken"];
+  const { majorCode, stdCode } = req.query;
+
+  try {
+    const [needUnit, unitYear] = getNeedUnit(majorCode, stdCode);
     // console.log(`checkGrades - getGenEd for ${majorCode}`)
-    console.log(`GetGenEd/ checkGrades of ${majorCode}: ${needUnit.Wellness} ${needUnit.Entrepreneurship} ${needUnit.Thai_Citizen_and_Global_Citizen} ${needUnit.Language_and_Communication} ${needUnit.Aesthetics}`)
+    console.log(
+      `GetGenEd/ checkGrades of ${majorCode}: ${needUnit.Wellness} ${needUnit.Entrepreneurship} ${needUnit.Thai_Citizen_and_Global_Citizen} ${needUnit.Language_and_Communication} ${needUnit.Aesthetics}`
+    );
     // console.log(needUnit['Wellness'])
-    
-    let result = await getResultBlock(needUnit)
+
+    let result = await getResultBlock(needUnit);
 
     const response = await axios.get(checkGradesLink, {
       headers: {
-        'app-key': 'txCR5732xYYWDGdd49M3R19o1OVwdRFc',
-        'x-access-token': accessToken
+        "app-key": "txCR5732xYYWDGdd49M3R19o1OVwdRFc",
+        "x-access-token": accessToken,
       },
       params: {
-        'idcode': stdCode
-      }
-    })
-    console.log("GetGenEd/ checkGrades code from ku:", response.data.code)
+        idcode: stdCode,
+      },
+    });
+    console.log("GetGenEd/ checkGrades code from ku:", response.data.code);
     if (!response.data.results) {
       console.log(response.data.message);
-    }
-    else {
-      for(const year of response.data.results){
-      // console.log(year.grade)
-        for(const sub of year.grade){
-        
+    } else {
+      for (const year of response.data.results) {
+        // console.log(year.grade)
+        for (const sub of year.grade) {
           // console.log(sub.subject_code, sub.grade)
-          const subject = getSubject(sub.subject_code)
+          const subject = getSubject(sub.subject_code);
           // console.log(subject)
-          if (sub.grade != 'W' && subject) {
-            result[subject.type].done += sub.credit
-            result[subject.type].subjects.push(sub)
+          if (sub.grade != "W" && subject) {
+            result[subject.type].done += sub.credit;
+            result[subject.type].subjects.push(sub);
           }
         }
       }
     }
-    
-    result.unitYear = +unitYear || 0
 
-    res.status(200).json(
-      result
-    )
-    console.log("GetGenEd/ Done. semester:", response.data.results?.length)
-  }
-  catch (e) {
-    console.log({
-      status: e.response.status,
-      data: e.response.data
-    })
-    try{
-      res.status(+e.response.status || 500).json({"msg": e.response.statusText})
-      console.log("GetGenEd/ Fail, success call ku api")
-    } catch (er) {
-      res.status(400).json({"msg": "fail to getGenEd"})
-      console.log("GetGenEd/ Fail, unsuccess call ku api")
-    }
-  }
-})
+    result.unitYear = +unitYear || 0;
 
-app.get('/getData', async (req, res) => {
-  try{
-    const response = await axios.get(sheetLink)
+    res.status(response.status).json(result);
+    console.log("GetGenEd/ Done Semester: ", response.data.results?.length);
+  } catch (error) {
+    console.error("GetGenEd Error:", error.message);
+
+    const status = error.response ? error.response.status : 500;
+    const message = error.response
+      ? error.response.statusText
+      : "Internal Server Error";
+
+    res.status(status).json({ message });
+    console.log("GetGenEd/ Failed");
+  }
+});
+
+app.post("/a", (req, res) => {
+  throw new Error("BROKEN"); // Express will catch this on its own.
+});
+
+app.get("/getData", async (req, res) => {
+  try {
+    const response = await axios.get(sheetLink);
     // console.log(response.data)
-    res.json(response.data)
-  } catch (e){
+    res.json(response.data);
+  } catch (e) {
     res.status(400).json({
-      "code": "Sheet/ Fail to get data",
-    })
+      code: "Sheet/ Fail to get data",
+    });
   }
-})
+});
 
-app.get('/getUpdatedNote', async (req, res) => {
-  console.log("GetUpdatedNote")
+app.get("/getUpdatedNote", async (req, res) => {
+  console.log("GetUpdatedNote");
   res.status(200).json({
-    "updated": [
+    updated: [
       "[Jun 22, 2022] update table to semester 2565-1",
       "[Jun 22, 2022] new subject: 01355119 ทักษะการอ่านภาษาอังกฤษที่จําเป็น Language_and_Communication",
       "[Jun 22, 2022] new subject: 01355109 ทักษะการฟัง-การพูดภาษาอังกฤษที่จําเป็น Language_and_Communication",
       "[Jun 22, 2022] new subject: 01418102 เทคโนโลยีสารสนเทศเพื่อผู้ประกอบการ Entrepreneurs",
       "[Jun 22, 2022] new subject: 01387105 พุทธจริยศาสตร์ในการดำเนินธุรกิจ Entrepreneurs",
-      "[Nov 29, 2021] new subject: 01132101 ผู้ประกอบการรุ่นใหม่ Entrepreneurs"
-    ]
-  })
-})
+      "[Nov 29, 2021] new subject: 01132101 ผู้ประกอบการรุ่นใหม่ Entrepreneurs",
+    ],
+  });
+});
 
-app.listen(process.env.PORT, () => console.log('Connected'))
+function errorHandler(err, req, res, next) {
+  if (res.headersSent) {
+    return next(err);
+  }
+  res.status(500).json({ error: err.message });
+}
+
+function logErrors(err, req, res, next) {
+  console.log("errrrrrr 22222");
+  console.error(err.stack);
+  next(err);
+}
+
+app.use(logErrors);
+app.use(errorHandler);
+
+app.listen(process.env.PORT, () => console.log("Connected"));
